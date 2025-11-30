500c6a342e9ad48fdd35dcb43eb34807d0e1006b
Dudeism74 <trustedessentialsgpt@gmail.com>
2025-11-30 01:07:41 +0000

    Remove leftover resolveDocumentActions.ts

--- studio/resolveDocumentActions.ts
+++
@@ -1,6 +0,0 @@
-// A minimal resolver that removes document actions to make the Studio read-only.
-import type { DefaultDocumentNodeResolver } from 'sanity/desk'
-
-export default function resolveDocumentActions() {
-  return []
-}
